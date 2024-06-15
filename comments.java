193734c1aad1b9eab8b4a3a8d8323d17704f00a0
Aditya Anil <127827576+aditya-anil-04@users.noreply.github.com>
2024-06-15 20:08:30 +0530

    Create comments.java

---
+++ comments.java
@@ -0,0 +1,14 @@
+public class comments {
+
+	public static void main (String args[]) {
+		
+		// Single Line comment
+		System.out.println("Single line comment above.");
+		
+		/* Multiple
+		 * Line
+		 * Comment */
+		System.out.println("Multi line comment above");
+	}
+	
+}
