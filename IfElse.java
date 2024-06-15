87ba1b88eec265e11f8b83130b38651674314383
Aditya Anil <127827576+aditya-anil-04@users.noreply.github.com>
2024-06-15 20:14:07 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,20 @@
+public class IfElse {
+
+	public static void main (String args[]) {
+		
+		int larger = 5;
+		int smaller = 4;
+		
+		if (larger > smaller) {
+			
+			System.out.println("True");
+			
+		} else {
+			
+			System.out.println("False");
+			
+		}
+		
+	}
+	
+}
