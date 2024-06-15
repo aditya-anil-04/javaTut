8b678e2b2630b4ec192deea703948bcddbaa7e7c
Aditya Anil <127827576+aditya-anil-04@users.noreply.github.com>
2024-06-15 20:12:34 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,11 @@
+public class Concatenation {
+
+	public static void main (String args[]) {
+		
+		String string1 = "word1";
+		String string2 = "word2";
+		System.out.println(string1 + string2);
+		
+	}
+	
+}
